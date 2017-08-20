0b9850c67537bb739c48b83c5ee94daa2612aa43
dornfelder <adressezumanmelden@gmx.de>
2017-08-20 20:43:42 +0200

    function gerMonthNames added in order to print german names of moths in latex table
    changes to some names in columnsDriver
    
    outData organized as dictionary
    
    summed costs for each driver
    
    toTemplate dictionary added. code for latex table hash to added
    
    improvement note added: driverData should be organiszed as dictionary and reused when toTemplate is created.

--- abrechnungCarsharing.py
+++ abrechnungCarsharing.py
@@ -13,6 +13,10 @@ carNames = ['aurCs108']
 #Gebe den Namen des Fahrerverzeichnisses ein
 fileNameDriverDirectory = '2017_06_fahrer.txt'
 
+
+#Gebe das Erstelldatum der Abrechnung ein
+settlementDate = '18 August 2017'
+
 #Ende Nutzereingabe
 ############################
 
@@ -42,6 +46,17 @@ def calculateCost( distance , duration):
 def calculateDuration( year, month, dateBeginStrZfilled2Numbers, timeBeginStrT4Numbers ):
     return datetime.strptime( str(year)+str(month).zfill(2)+ dateBeginStrZfilled2Numbers + timeBeginStrT4Numbers,'%Y%m%d%H%M' )
 
+def gerMonthNames(integerMonth):
+    #Gebe den deutschen Monatsbezeichner als String zurueck
+    #Erstelle eine Liste der deutschen Monatsbezeichner, da die lokalen Datumsangaben der Bibliothek Datetime Platformabhaengig sind.
+    gerMonthsNamesList = ['Januar', 'Februar', 'März' ,'April', 'Mai','Juni','Juli','August','September','Oktober','November','Dezember']
+    if (1 <= integerMonth) & (integerMonth <=12):
+        tmp = gerMonthsNamesList[integerMonth-1]
+    else:
+        sys.exit('Der Parameter zu gerMonthNames ist kein Integer im Bereich 1 bis 12')
+    return tmp
+
+
 #Ende Definition Funktionen
 ############################
 
@@ -67,7 +82,7 @@ columns = {
 #Lese Daten aus den Eingabedateien ein
 inputData = []
 for index,filename in enumerate(fileNames):
-    with open(filename,'r',newline='') as myFile:
+    with open(filename,'r', newline='') as myFile:
         next(myFile)    #Skip Header
         myReader = csv.reader(myFile, delimiter = ';')
         #Strip input, d.h. entferne Leerzeichen und Zeilenspruenge aus den Strings
@@ -76,24 +91,25 @@ for index,filename in enumerate(fileNames):
             inputData[-1].append(carNames[index])   #Fuege den Aktuellen Fahrzeugnamen zu jedem Eintrag in inputData hinzu
 
 #Definiere Indices fuer verschiedene Datenbestandteile in dem Fahrerverzeichnis
+#!#ImprovementNote:driverData should be organiszed as dictionary and reused when toTemplate is created.
 columnsDriver = {
 'driverName'    :0,
-'forename'      :1,
-'lastname'      :2,
+'firstName'     :1,
+'lastName'      :2,
 'street'        :3,
-'street number' :4,
+'streetNumber'  :4,
 'postcode'      :5,
-'town'          :6
+'city'          :6
 }
 
 #Lese das Fahrerverzeichnis ein
-driverData = []
+driverData = {}
 with open(fileNameDriverDirectory,'r',newline = '') as myFile:
     next(myFile)    #Skip Header
     myReader = csv.reader(myFile,delimiter = ';')
     #Strip input, d.h. entferne Leerzeichen und Zeilenspruenge aus den Strings
     for data in myReader:
-        driverData.append( [x.strip() for x in data] )
+        driverData[data[0]] = [x.strip() for x in data]
 
 #Ende Einlesen der Rohdaten
 ############################
@@ -113,7 +129,7 @@ for data in inputData:
     distance = int(data[ columns['distance'] ])
     cost = calculateCost( distance , duration )
     
-    calcData.append([name, begin, carName, duration, distance, cost])
+    calcData.append([name, begin, carName, duration, distance, cost])   #cost is last entry
 
 #Ordne die einzelnen Fahrteintraege den Fahrern zu
 
@@ -123,13 +139,44 @@ calcData.sort(key = lambda x: (x[0] , x[1] ) )
 #Ersetze den Eintrag begin (datetime object) mit einem String
 for data in calcData:
     data[1] = data[1].strftime('%d %m %Y')
-#Erstelle eine Liste der im betrachteten Monat aktiven Fahrer ausgehend von den Fahrdaten
-uniqueDrivers = set( map(lambda x:x[0], calcData) )
 
-#Erstelle eine Ausgabeliste, die fuer jeden Fahrer eine Liste der Eintraege in enthaelt. Diese Liste der Eintraege ist schon geordnet
-outData = []
+#Erstelle eine Liste der im betrachteten Monat aktiven Fahrer ausgehend von den Fahrdaten
+uniqueDrivers = list(set( map(lambda x:x[0], calcData) ))
+uniqueDrivers.sort()
+
+#Erstelle eine Ausgabeliste, die fuer jeden Fahrer eine Liste der Eintraege enthaelt. 
+#Diese Liste der Eintraege ist schon geordnet
+outData = {}
 for driver in uniqueDrivers:
-    outData.append( [ x for x in calcData if x[0] == driver ] )
+    outData[driver] =  [ x for x in calcData if x[0] == driver ]
+
+#Summiere die Kosten jedes Fahrers
+toTemplateSums = {}
+for driver in uniqueDrivers:
+    tmp = 0
+    for data in outData[driver]:
+        tmp = tmp + data[-1]    #cost is last entry
+    toTemplateSums[driver] = tmp
+
+#Erstelle ein Dictinonary von Dictionaries um Platzhalter in der Vorlage zu ersetzen
+toTemplate = {}
+for driver in uniqueDrivers:
+    tmp = {}
+    tmp['month']    =   (gerMonthNames(month))
+    tmp['year']     =   str(year)
+    tmp['sum']      =   str(toTemplateSums[driver])
+    tmp['firstName']=   driverData[driver][columnsDriver['firstName']]
+    tmp['lastName'] =   driverData[driver][columnsDriver['lastName']]
+    tmp['street'] =   driverData[driver][columnsDriver['street']]
+    tmp['streetNumber'] =   driverData[driver][columnsDriver['streetNumber']]
+    tmp['postcode'] =   driverData[driver][columnsDriver['postcode']]
+    tmp['city'] =   driverData[driver][columnsDriver['city']]
+
+    #table data has to be added.
+    toTemplate[driver] = tmp 
+
+
+
 
 #Ende Bearbeitung der Daten
 ############################
